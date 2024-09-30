5217f5e10f7c49fa3a6e4760a1ec2308b6db72fd
BenIzak <benizakahamadi@gmail.com>
2024-10-01 01:34:41 +0200

    test: deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React V 1.0</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is not {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
